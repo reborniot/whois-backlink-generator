7fd18ebef8f4c88787de70489bf426ce7bae40af
Ibrahim Nergiz <phyesix@gmail.com>
2021-10-23 23:39:28 +0300

    Change title

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var siteLinkUl = document.querySelector("body > div.container > div.process-screen > ul");
-var calistirButton = document.querySelector("body > div.container > div.button > h2");
-
 let whoIsLinkList = [
     'https://www.alexa.com/siteinfo/',
     'https://besthostingprice.com/whois/',
@@ -58,46 +55,9 @@ let whoIsLinkList = [
 ];
 
 document.querySelector('.js-start').addEventListener('click', function(e){
-    const domain = e.currentTarget;
+    const domain = document.querySelector('.js-sitename');
     whoIsLinkList.forEach(link => {
         let whoIsLinkWithDomain = link + domain;
         window.open(whoIsLinkWithDomain, scrollbars="yes",width=800,height=800);
     });
 });
-
-
-calistirButton.addEventListener("click", function(e){
-
-    let i = 0; // döngü sayısını tanımlıyoruz
-        function loop() { // bir fonksiyon oluşturuyoruz
-            if (i < siteLinkUl.children.length) { // eğer i, children.length'ten küçük ise... 
-                window.open(siteLinkUl.children[i].firstElementChild, scrollbars="yes",width=800,height=800);
-                siteLinkUl.children[i].setAttribute("class","clicked");
-                // kodlar buraya...
-                i++ // döngü sayısını arttırıyoruz
-                setTimeout(() => { // 3 saniye timeout koyuyoruz
-                loop(); // bu fonksiyonu bir daha çalıştırıyoruz.
-                }, 5000);
-        }
-        if(i >= siteLinkUl.children.length){
-            alert("İşlem Tamamlandı. Toplamda" + siteLinkUl.children.length + "tane whois backlink yapıldı");
-        }
- 
-}
-
-loop(); // en başta fonksiyonu çalıştırıyoruz
-
-});
-
-
-
-/*ÖNEMLİ NOTLAR*/
-
-    /*
-    for(let i=0; i < siteLinkUl.children.length; i++ ){
-        window.open(siteLinkUl.children[i].firstElementChild);
-        siteLinkUl.children[i].setAttribute("class","clicked");
-    }*/
-/*
-For döngüsü durdurulamıyor bu yüzden kendi döngümüzü yazmak zorunda kaldık..
-*/
